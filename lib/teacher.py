c68ad60469c2f62a146b7a0bec987343289c868f
Natalie Burgei <natalieburgei04@gmail.com>
2024-01-08 17:43:03 +0300

    Done

--- lib/teacher.py
+++ lib/teacher.py
@@ -3,19 +3,21 @@
 from user import User
 
 import random
-knowledge = [
-    "str is a data type in Python",
-    "programming is hard, but it's worth it",
-    "JavaScript async web request",
-    "Python function call definition",
-    "object-oriented teacher instance",
-    "programming computers hacking learning terminal",
-    "pipenv install pipenv shell",
-    "pytest -x flag to fail fast",
-]
+
 class Teacher(User):
-    def __init__(self, first_name, last_name, knowledge=knowledge):
+
+    def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
-        self.knowledge = knowledge
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
+
     def teach(self):
-       return self.knowledge[random.randint(0,len(self.knowledge)-1)]
+        return self.knowledge[random.randint(0, len(self.knowledge) - 1)]
